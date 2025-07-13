bb062d92b260841117cb92cd5dc898db9a0ba6ce
Keerthana S <132582064+S-KeerthanaDharshini@users.noreply.github.com>
2025-07-13 23:03:55 +0530

    Add files via upload

---
+++ increament.js
@@ -0,0 +1,9 @@
+//increment and decrement
+
+let bonusPoints = 50;
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints)
+bonusPoints = bonusPoints -75
+console.log(bonusPoints)
+bonusPoints = bonusPoints +45
+console.log(bonusPoints)

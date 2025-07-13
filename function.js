187555bd1880284c7a2161025d76749c7f232edb
Keerthana S <132582064+S-KeerthanaDharshini@users.noreply.github.com>
2025-07-13 23:03:40 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,33 @@
+function print(){
+    console.log(42)
+}
+print()
+
+//racer lapping time
+
+/* remove this
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function laps(){
+    console.log(lap1 + lap2+lap3)
+}
+laps()
+
+//laps completed 
+
+let lapsCompleted = 0
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+remove this*/
+let lapsCompleted = 0
+function lapsCompletedLog(){
+    lapsCompleted++
+    console.log(lapsCompleted)
+}
+lapsCompletedLog()
+lapsCompletedLog()
+lapsCompletedLog()

ff44abb131ff6a809c3cd0628e41d39410ec8a96
Keerthana S <132582064+S-KeerthanaDharshini@users.noreply.github.com>
2025-07-14 16:41:20 +0530

    Add files via upload

---
+++ jsString.js
@@ -0,0 +1,21 @@
+/* let message = "You have three new notifications"
+console.log(message)
+
+let userName = "keerthana"
+let messageToUser = "You have logged out"
+console.log(userName+", "+messageToUser+"!")
+
+
+
+let userName = "keerthana"
+let greeting ="Hi, my name is"
+let myGreeting = greeting +" " + userName 
+console.log(myGreeting)
+
+*/
+
+
+console.log(4 + 5)  //9
+console.log("2" + "4") //24
+console.log("5" + 1)  //51
+console.log(100 + "100") //100100

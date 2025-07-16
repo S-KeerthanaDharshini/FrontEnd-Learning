388f9cd1f42253bde7c592abf35614312071b14c
Keerthana S <132582064+S-KeerthanaDharshini@users.noreply.github.com>
2025-07-16 19:05:20 +0530

    Add files via upload

---
+++ CompositeArray.js
@@ -0,0 +1,13 @@
+//Composite Array ----> Array with mixed datatype
+
+//Task : Array - ordered list of items - coposite / complex data type
+
+// Create an array that describes yourself. Use the three primitive data types you've learned
+// It should contain your name (string), your age (number), and whether you like pizza (boolean)
+
+
+let aboutMe = ["keerthana" , 21 , true]
+
+for(let i=0;i<aboutMe.length;i++){
+    console.log(aboutMe[i])
+}

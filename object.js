d48a787df00265e56a7ef099f8d408beb13401cd
Keerthana S <132582064+S-KeerthanaDharshini@users.noreply.github.com>
2025-07-16 23:11:21 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,13 @@
+// Create an object that represents an airbnb castle listing.
+// It should contain at least one boolean, one string, one number, and one array
+// Log out at least two of the keys using the dot notation
+
+let airbnb ={
+    name : "Dairsale Castle",
+    price :1089,
+    guests : [14,6,10,5],
+    fiveStar :false
+}
+
+console.log(airbnb.name)
+console.log(airbnb.guests[2])

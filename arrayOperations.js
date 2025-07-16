40e1e7c9a6806a617f4f0b1037e690bdf97b1bcf
Keerthana S <132582064+S-KeerthanaDharshini@users.noreply.github.com>
2025-07-16 19:04:25 +0530

    Add files via upload

---
+++ arrayOperations.js
@@ -0,0 +1,19 @@
+
+// Push the newMessage to the messages array, and then log out the array
+
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+
+let newMessage = "Same here!"
+
+messages.push(newMessage)
+
+console.log("Before")
+console.log(messages)
+console.log("After")
+messages.pop()
+
+console.log(messages)

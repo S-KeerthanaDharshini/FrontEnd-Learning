f70fe997ad1dfb52bc370495dc1a1e2519403773
Keerthana S <132582064+S-KeerthanaDharshini@users.noreply.github.com>
2025-07-16 23:42:13 +0530

    Add files via upload

---
+++ greeting/index.js
@@ -0,0 +1,14 @@
+let sentence = ["Hello ", "my ", "name ", "is ", "Per"] 
+let greetingEl = document.getElementById("greeting-el")
+
+// Render the sentence in the greetingEl paragraph using a for loop and .textContent
+
+for(let i =0;i<sentence.length;i++){
+    greetingEl.textContent += sentence[i]
+}
+//if space is not given in tht array
+
+
+for(let i =0;i<sentence.length;i++){
+    greetingEl.textContent += sentence[i]+" "
+}

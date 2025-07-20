c1b33624a6028e80b3de8f402c7672e3caf28599
Keerthana S <132582064+S-KeerthanaDharshini@users.noreply.github.com>
2025-07-20 21:16:45 +0530

    Add files via upload

---
+++ template String.js
@@ -0,0 +1,14 @@
+// template strings/literals
+
+const recipient = "James"
+// Create a new variable, sender, and set its value to your name
+
+// Use your sender variable instead of "Per"
+
+// Refactor the email string to use template strings
+const sender = "keerthana"
+const email = `Hey ${recipient} ! 
+How is it going? 
+Cheers ${sender}`
+
+console.log(email)

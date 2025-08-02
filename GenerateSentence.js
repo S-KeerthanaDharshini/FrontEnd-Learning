0be36d5bcb169c791d33966b0718a76c613eb8ba
Keerthana S <132582064+S-KeerthanaDharshini@users.noreply.github.com>
2025-08-02 19:30:37 +0530

    Add files via upload

---
+++ GenerateSentence.js
@@ -0,0 +1,35 @@
+// The generateSentence(desc, arr) takes two parameterer: a description and an array.
+// It should return a string based upon the description and array.
+
+// Example 1: if you pass in "largest countries",and ["China", "India", "USA"],
+// it should return the string: "The 3 largest countries are China, India, USA"
+
+// Example 2:If you pass in "best fruits" and ["Apples", "Bananas"], it should return:
+// "The 2 best fruits are Apples, Bananas"
+
+// Use both a for loop and a template string to solve the challenge
+ let desc ="largest countries"
+
+let arr =  ["China", "India", "USA"]
+
+// function generateSentence(desc, arr) {
+//     let words =""
+   
+//     for(let i=0;i<arr.length-1;i++){
+//         words+=arr[i]+", "
+//     }
+//     console.log("The "+arr.length+" "+desc+" are "+words+" "+arr[arr.length-1])
+// }
+function generateSentence(desc,arr){
+let words =""
+let lastIndex = arr.length-1;
+     for(let i=0;i<arr.length-1;i++){
+        if(i ===lastIndex){
+            words+=arr[i]
+        }
+        words+=arr[i]+", "
+   }
+
+console.log(`The ${arr.length} ${desc} are ${words}${arr[arr.length-1]}`)
+}
+generateSentence(desc, arr)
